adc1205388e55260f8277a397b0b7c1711c08dac
podup <podup@podups-MacBook-Air.local>
2026-02-01 01:15:29 +0530

    initial commit

---
+++ src/main/java/JavaBS/Basics/Student.java
@@ -0,0 +1,11 @@
+package JavaBS.Basics;
+
+public class Student {
+    public static void main(String[] args){
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
+
+        System.out.println(s1==s3);
+    }
+}
